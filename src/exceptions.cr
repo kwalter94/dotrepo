b38edef62ccf47f4963ac9093597de91259fe411
kwalter <walterkaunda@live.co.uk>
2021-09-11 21:06:12 +0200

    Implemented file export.

--- src/exceptions.cr
+++ src/exceptions.cr
@@ -2,4 +2,6 @@ module Dotfiles::Exceptions
   class Exception < ::Exception; end
 
   class ImportFailed < Exception; end
+
+  class ExportFailed < Exception; end
 end
--- src/file_exporter.cr
+++ src/file_exporter.cr
@@ -1,3 +1,44 @@
+require "./exceptions"
+require "./utils"
+
+require "file_utils"
+require "path"
+
 module Dotfiles::FileExporter
-  def self.export_file(relative_dotfile_path); end
+  ##
+  # Imports dotfile from the dotfiles repository.
+  #
+  # For example `Dotfiles::FileExporter.export_file(".config/fish/config.fish"])`
+  # creates a symlink at ~/.config/fish/config.fish to the corresponding file in
+  # the dotfiles repository.
+  def self.export_file(relative_dotfile_path : String)
+    export_file(Path[relative_dotfile_path])
+  end
+
+  ##
+  # Imports dotfile from the dotfiles repository.
+  #
+  # For example `Dotfiles::FileExporter.export_file(Path[".config", "fish", "config.fish"])`
+  # creates a symlink at ~/.config/fish/config.fish to the corresponding file in
+  # the dotfiles repository.
+  def self.export_file(relative_dotfile_path : Path)
+    dotfile_path = expand_dotfile_path(relative_dotfile_path)
+    unless File.exists?(dotfile_path)
+      raise Exceptions::ExportFailed.new("Dotfile #{relative_dotfile_path} does not exist")
+    end
+
+    export_path = dotfile_export_path(relative_dotfile_path)
+    Dir.mkdir_p(export_path.dirname)
+    FileUtils.ln_s(dotfile_path, export_path)
+
+    export_path
+  end
+
+  def self.expand_dotfile_path(relative_dotfile_path : Path)
+    Utils.repository_path.join(relative_dotfile_path)
+  end
+
+  def self.dotfile_export_path(relative_dotfile_path : Path)
+    Path.home.join(relative_dotfile_path)
+  end
 end
