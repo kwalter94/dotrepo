5b2c68ae7c2b7e9ff9304550d8d810472e1894ee
kwalter <walterkaunda@live.co.uk>
2021-09-11 20:10:19 +0200

    Added unit tests for file_exporter.
    - Checks whether exported file is a symlink
    - Checks whether relative directory structure is maintained on export

---
+++ spec/file_exporter_spec.cr
@@ -0,0 +1,44 @@
+require "./spec_helper"
+
+require "../src/file_exporter"
+
+require "dir"
+require "file"
+require "path"
+
+describe Dotfiles::FileExporter do
+  before_each do
+    Dir.mkdir_p(repository_path)
+  end
+
+  after_each do
+    Dir.delete_tree(repository_path)
+  end
+
+  describe :export_file do
+    it "creates a symbolic link to a file in .dotfiles folder" do
+      dotfile_path = File.tempfile(dir: repository_path.to_s, &.print("Hello")).path
+      export_filename = Path[dotfile_path].basename
+      exported_path = Path.home.join(export_filename)
+
+      Dotfiles::FileExporter.export_file(export_filename)
+      File.symlink?(exported_path).should be_true
+      File.readlink(exported_path).should eq(repository_path.join(export_filename).to_s)
+    end
+
+    it "maintains relative directory structure for exported paths" do
+      dotfile_dir_path = repository_path.join("some", "relative", "path")
+      Dir.mkdir_p(dotfile_dir_path)
+
+      pp(path: dotfile_dir_path.to_s, exists: Dir.exists?(dotfile_dir_path))
+
+      dotfile_path = File.tempfile(dir: dotfile_dir_path.to_s, &.print("Hello")).path
+      export_filename = Path[dotfile_path].basename
+      exported_path = Path.home.join("some", "relative", "path", export_filename)
+
+      Dotfiles::FileExporter.export_file("some/relative/path/#{export_filename}")
+      Dir.exists?(exported_path.dirname).should eq(true)
+      Path[File.readlink(exported_path)].should eq(Path[dotfile_path])
+    end
+  end
+end
---
+++ src/file_exporter.cr
@@ -0,0 +1,3 @@
+module Dotfiles::FileExporter
+  def self.export_file(relative_dotfile_path); end
+end
