ee1e7eb27c364c3274854368f1caefbdb0cc0ae6
kwalter <walterkaunda@live.co.uk>
2021-09-11 20:19:52 +0200

    Made Path the preferred type for paths in file_importer
    Was using the less flexible and less obvious String type.

--- src/file_importer.cr
+++ src/file_importer.cr
@@ -9,7 +9,7 @@ module Dotfiles::FileImporter
     import(Path[path])
   end
 
-  def self.import(path : String)
+  def self.import(path : Path)
     dotfile_path = self.dotfile_path(path)
     Dir.mkdir_p(dotfile_path.dirname)
 
@@ -21,7 +21,7 @@ module Dotfiles::FileImporter
     raise Exceptions::ImportFailed.new("Failed to import file: #{path}", error)
   end
 
-  def self.dotfile_path(path : String)
+  def self.dotfile_path(path : Path)
     stripped_path = self.strip_home_from_path(path)
 
     if stripped_path == path
@@ -31,8 +31,8 @@ module Dotfiles::FileImporter
     Utils.repository_path.join(stripped_path)
   end
 
-  def self.strip_home_from_path(path : String)
-    expanded_path = Path[path].expand.to_s
-    expanded_path.gsub(/^#{Path.home}/, "")
+  def self.strip_home_from_path(path : Path)
+    expanded_path = path.expand.to_s
+    Path[expanded_path.gsub(/^#{Path.home}/, "")]
   end
 end
